e421a31ca03f23ae4e9ee26f5aeba8a0bf3dcaf1
Robin Ruzza <robin.ruzza@roche.com>
2022-05-23 09:18:38 +0200

    Add project files.

---
+++ M151/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(M151.Startup))]
+namespace M151
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
