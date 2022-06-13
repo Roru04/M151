82a9be9aa4df9d76e6ca77cb938856b85de71835
Julia <jsorrentino@online.gibz.ch>
2022-06-13 08:33:12 +0200

    Added Post Class

---
+++ M151.Shared/Models/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace M151.Data.Models
+{
+    public class Post
+    {
+        [Required]
+        public string Title { get; set; }
+
+
+        public int Id { get; set; }
+
+
+        public string ApplicationUserId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+
+        public int Likes { get; set; }
+
+        public int Comments { get; set; }
+
+        public string ApplicationUserName { get; set; }
+
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+
+        [Timestamp] public Byte[] RowVersion { get; set; }
+
+
+        public DateTime date { get; set; }
+    }
+}
